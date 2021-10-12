f84f914b37cc8890888f7472733ef8d9c52c9e55
songzhipeng <532355638@qq.com>
2021-10-12 15:47:40 +0800

    init

---
+++ src/test/java/com/szp/springlearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szp.springlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
