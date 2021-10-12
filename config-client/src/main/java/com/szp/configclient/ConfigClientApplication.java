89d267c0dada8323bda4399fc67892b4bc554029
causeThenEffect <songzhipeng123456@gmail.com>
2021-10-13 00:13:19 +0800

    test

--- config-client/src/main/java/com/szp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/szp/configclient/ConfigClientApplication.java
@@ -3,9 +3,14 @@ package com.szp.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author songzhipeng
+ */
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+//@EnableEurekaClient
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
