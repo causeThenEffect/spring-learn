3f73dcfa75b8f9e6411b17276c38ca4ee92517d3
causeThenEffect <songzhipeng123456@gmail.com>
2021-10-17 23:45:36 +0800

    page 使用

---
+++ page-test/src/test/java/com/szp/pagetest/PageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szp.pagetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
