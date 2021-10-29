368088031c65862f1f55570f4e6b88083ff8318c
causeThenEffect <songzhipeng123456@gmail.com>
2021-10-29 09:53:46 +0800

    Mybatis spring boot

--- mybatis-spring-boot/src/main/java/com/szp/mybatis/springboot/MybatisSpringBootApplication.java
+++ mybatis-spring-boot/src/main/java/com/szp/mybatis/springboot/MybatisSpringBootApplication.java
@@ -3,6 +3,9 @@ package com.szp.mybatis.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author songzhipeng
+ */
 @SpringBootApplication
 public class MybatisSpringBootApplication {
 
