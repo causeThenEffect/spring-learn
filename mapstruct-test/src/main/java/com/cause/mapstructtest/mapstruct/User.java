45b1e3c1fe671bcde4b62ded11b30cddc2d807a4
causeThenEffect <songzhipeng123456@gmail.com>
2022-03-26 06:21:15 +0800

    mapstruct test

--- mapstruct-test/src/main/java/com/cause/mapstructtest/mapstruct/User.java
+++ mapstruct-test/src/main/java/com/cause/mapstructtest/mapstruct/User.java
@@ -6,25 +6,10 @@ import lombok.Data;
  * @author cause
  * @date 2022/3/24
  */
+@Data
 public class User {
 
   private String id;
   private String name;
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 
 }
--- mapstruct-test/src/main/java/com/cause/mapstructtest/mapstruct/UserDTO.java
+++ mapstruct-test/src/main/java/com/cause/mapstructtest/mapstruct/UserDTO.java
@@ -7,23 +7,8 @@ import lombok.Data;
  * @author cause
  * @date 2022/3/24
  */
+@Data
 public class UserDTO {
   private String name;
   private String age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getAge() {
-    return age;
-  }
-
-  public void setAge(String age) {
-    this.age = age;
-  }
 }
